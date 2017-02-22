85abf23fe744f1bbaacf7fdfbf2b7742de78fecc
hachtman <jackfllr1@googlemail.com>
2017-02-22 16:21:24 +0000

    adding scss file structure and ayour to dashboard

--- src/js/controllers/dashboard/newsfeed.controller.js
+++ src/js/controllers/dashboard/newsfeed.controller.js
@@ -15,6 +15,7 @@ function newsfeedCtrl($http, API) {
       data: vm.tickers
     }).then(function successCallback(response) {
       vm.newsItems = response.data.newsItems;
+      parseNewsItemPubDates(vm.newsItems);
     }, function errorCallback(error) {
       console.log(error);
     });
@@ -33,6 +34,27 @@ function newsfeedCtrl($http, API) {
     });
   }
 
+  function parseNewsItemPubDates(array) {
+    for(var i = 0; i < vm.newsItems.length; i++) {
+      temp = vm.newsItems[i].pubDate.split(',')[1];
+      temp = temp.split(' ');
+      temp = `${temp[1]} ${temp[2]} ${temp[4]}`;
+      vm.newsItems[i].pubDate = temp;
+    }
+  }
+
+//Possible overscroll prevention
+  function preventOverscroll() {
+    tile = document.getElementsByClassName('scrollable')[0];
+    console.log(tile);
+    tile.bind('mousewheel', (e) => {
+      angular.element(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY);
+      //prevent page fom scrolling
+      return false;
+
+    });
+  }
+  // preventOverscroll()
   getWatchlistRSS();
   getSecRSS();
 }
