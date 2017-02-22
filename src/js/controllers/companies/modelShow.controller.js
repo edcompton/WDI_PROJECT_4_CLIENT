80831de871b6367fedf79a1f2b81fe89b54c6693
edcompton <eahcompton@gmail.com>
2017-02-22 17:42:07 +0000

    added a navbar

--- src/js/controllers/companies/modelShow.controller.js
+++ src/js/controllers/companies/modelShow.controller.js
@@ -16,19 +16,9 @@ function CompanyModelShowCtrl($http, API){
       url: `${API}/companies/model/${ticker}`
     }).then(function successCallback(response) {
       console.log(response);
-      vm.is0 = response.data.is_yearly_results[0];
-      vm.is1 = response.data.is_yearly_results[1];
-      vm.is2 = response.data.is_yearly_results[2];
-      vm.is = [vm.is2, vm.is1, vm.is0];
-
-      vm.bs0 = response.data.bs_yearly_results[0];
-      vm.bs1 = response.data.bs_yearly_results[1];
-      vm.bs = [vm.bs1, vm.bs0];
-
-      vm.cf0 = response.data.cf_yearly_results[0];
-      vm.cf1 = response.data.cf_yearly_results[1];
-      vm.cf2 = response.data.cf_yearly_results[2];
-      vm.cf = [vm.cf2, vm.cf1, vm.cf0];
+      vm.is =  response.data.is_yearly_results;
+      vm.bs = response.data.bs_yearly_results;
+      vm.cf = response.data.cf_yearly_results;
 
 
     }, function errorCallback(error) {
