9370c92716e124f07ee3cf6aa81a79145c642b38
HenryDavies <henrywdavies@gmail.com>
2017-02-23 15:46:13 +0000

    linked sector and description

--- src/js/controllers/companies/companyData.controller.js
+++ src/js/controllers/companies/companyData.controller.js
@@ -7,8 +7,9 @@ function CompanyDataCtrl($http, API){
   const vm = this;
   const ticker = 'AAPL';
 
-  if (!vm.fs) getCompanyData();
-
+  getCompanyData();
+  getSectorDescription();
+  getEpsEstimates();
   getHistoricalPrices();
 
   function getHistoricalPrices() {
@@ -36,7 +37,6 @@ function CompanyDataCtrl($http, API){
       console.log(error);
     });
   }
-  getEpsEstimates();
 
 
   function getCompanyData() {
@@ -44,26 +44,25 @@ function CompanyDataCtrl($http, API){
       method: 'GET',
       url: `${API}/companies/model/${ticker}`
     }).then(function successCallback(response) {
-      vm.fs = {};
-      vm.fs.is0 = response.data.is_yearly_results[0];
-      vm.fs.is1 = response.data.is_yearly_results[1];
-      vm.fs.is2 = response.data.is_yearly_results[2];
-      vm.fs.is = [vm.fs.is2, vm.fs.is1, vm.fs.is0];
-
-      vm.fs.bs0 = response.data.bs_yearly_results[0];
-      vm.fs.bs1 = response.data.bs_yearly_results[1];
-      vm.fs.bs = [vm.fs.bs1, vm.fs.bs0];
-
-      vm.fs.cf0 = response.data.cf_yearly_results[0];
-      vm.fs.cf1 = response.data.cf_yearly_results[1];
-      vm.fs.cf2 = response.data.cf_yearly_results[2];
-      vm.fs.cf = [vm.fs.cf2, vm.fs.cf1, vm.fs.cf0];
-
-      console.log(vm.fs);
+      console.log(response);
+      vm.is =  response.data.is_yearly_results;
+      vm.bs = response.data.bs_yearly_results;
+      vm.cf = response.data.cf_yearly_results;
     }, function errorCallback(error) {
       console.log(error);
     });
   }
 
-
+  function getSectorDescription() {
+    $http({
+      method: 'GET',
+      url: `${API}/companies/info/${ticker}`
+    }).then(function successCallback(response) {
+      vm.companyName = response.data.name;
+      vm.companyDescription = response.data.description;
+      vm.companySector = response.data.sector;
+    }, function errorCallback(error) {
+      console.log(error);
+    });
+  }
 }
